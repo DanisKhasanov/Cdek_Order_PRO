58eb06f52061803a0bad30bbb73911a0305958a3
DanisKhasanov <danis.khasanov@outlook.com>
2024-08-05 17:49:32 +0300

    обновил инпут ФИО

--- src/components/orderForm/order/Order.tsx
+++ src/components/orderForm/order/Order.tsx
@@ -8,16 +8,17 @@ import Order from "../../../api/testData"; // фейковые данные
 import { RootState } from "../../../store/store";
 import { validationSchema } from "./Validation";
 import { GetOrderData } from "../../../api/GetOrderData";
-import { AddressSuggestions } from "react-dadata";
+import { AddressSuggestions, FioSuggestions } from "react-dadata";
 import { StyledInput } from "../styles/StyleInputAddressOrder";
 import "react-dadata/dist/react-dadata.css";
+import { ClipLoader } from "react-spinners";
 
 const OrderForm = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const orderData = useSelector((state: RootState) => state.orderForm);
   const apiKey = import.meta.env.VITE_DADATA_API_KEY;
-
+  const [loading, setLoading] = useState(true);
   const CustomInput = forwardRef((props, ref: any) => (
     <StyledInput {...props} ref={ref} />
   ));
@@ -32,7 +33,6 @@ const OrderForm = () => {
             name: order.recipient.name,
             phones: order.recipient.phones,
           },
-
           comment: order.comment,
           cod: order.cod,
           sum: order.sum,
@@ -40,6 +40,8 @@ const OrderForm = () => {
       );
     } catch (error) {
       console.error("Ошибка при загрузке данных заказа", error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -56,118 +58,130 @@ const OrderForm = () => {
 
   return (
     <div className="order-form">
-      <Formik
-        enableReinitialize
-        initialValues={orderData}
-        validationSchema={validationSchema}
-        onSubmit={onSubmit}
-      >
-        {({ errors, touched, setFieldValue }) => (
-          <Form>
-            <div className="form-group">
-              <label htmlFor="account">* Договор СДЕК:</label>
-              <Field
-                as="select"
-                id="account"
-                name="account"
-                className={`form-control ${
-                  errors.account && touched.account ? "error" : ""
-                }`}
-                placeholder={
-                  errors.account && touched.account ? errors.account : ""
-                }
-              >
-                <option value="" label="Выберите договор" hidden />
-                <option value="GRM" label="ГРМ" />
-                <option value="ZAR" label="ЗАР" />
-              </Field>
-            </div>
+      {loading ? (
+        <div className="loading-container">
+          <ClipLoader color={"#000"} loading={loading} size={25} />
+          <p>Загрузка...</p>
+        </div>
+      ) : (
+        <Formik
+          enableReinitialize
+          initialValues={orderData}
+          validationSchema={validationSchema}
+          onSubmit={onSubmit}
+        >
+          {({ errors, touched, setFieldValue }) => (
+            <Form>
+              <div className="form-group">
+                <label htmlFor="account">* Договор СДЕК:</label>
 
-            <div className="form-group">
-              <label htmlFor="recipient.name">* Имя получателя:</label>
-              <Field
-                type="text"
-                id="recipient.name"
-                name="recipient.name"
-                className={`form-control ${
-                  errors.recipient?.name && touched.recipient?.name
-                    ? "error"
-                    : ""
-                }`}
-                placeholder={
-                  touched.recipient?.name && errors.recipient?.name
-                    ? errors.recipient.name
-                    : "Например: Иван Иванов Иванович"
-                }
-              />
-            </div>
+                <Field
+                  as="select"
+                  id="account"
+                  name="account"
+                  className={`form-control ${
+                    errors.account && touched.account ? "error" : ""
+                  }`}
+                  placeholder={
+                    errors.account && touched.account ? errors.account : ""
+                  }
+                >
+                  <option value="" label="Выберите договор" hidden />
+                  <option value="GRM" label="ГРМ" />
+                  <option value="ZAR" label="ЗАР" />
+                </Field>
+              </div>
 
-            <div className="form-group">
-              <label htmlFor="recipient.phones[0].number">
-                * Номер телефона:
-              </label>
-              <Field
-                type="text"
-                id="recipient.phones[0].number"
-                name="recipient.phones[0].number"
-                className={`form-control ${
-                  errors.recipient?.phones?.[0] &&
-                  touched.recipient?.phones?.[0]?.number
-                    ? "error"
-                    : ""
-                }`}
-                placeholder={
-                  touched.recipient?.phones?.[0]?.number &&
-                  errors.recipient?.phones?.[0]
-                    ? "Номер телефона обязателен"
-                    : 'Например: "+7 (999) 999-99-99"'
-                }
-              />
-            </div>
+              <div className="form-group">
+                <label htmlFor="recipient.name">* Имя получателя:</label>
+                <div
+                  className={`form-control address ${
+                    errors.recipient?.name && touched.recipient?.name
+                      ? "error"
+                      : ""
+                  }`}
+                >
+                  <FioSuggestions
+                    token={apiKey}
+                    onChange={(suggestion: any) => {
+                      setFieldValue("recipient.name", suggestion.value);
+                    }}
+                    inputProps={{
+                      placeholder: "Введите имя",
+                    }}
+                    defaultQuery={orderData.recipient.name}
+                    customInput={CustomInput}
+                  />
+                </div>
+              </div>
 
-            <div className="form-group">
-              <label htmlFor="to_location.address">* Адрес получателя:</label>
-              <div
-                className={`form-control address ${
-                  errors.to_location?.address && touched.to_location?.address
-                    ? "error"
-                    : ""
-                }`}
-              >
-                <AddressSuggestions
-                  token={apiKey}
-                  onChange={(suggestion: any) => {
-                    setFieldValue("to_location.city", suggestion.data.city);
-                    setFieldValue(
-                      "to_location.postal_code",
-                      suggestion.data.postal_code
-                    );
-                    setFieldValue("to_location.address", suggestion.value);
-                  }}
-                  inputProps={{
-                    placeholder: "Введите адрес",
-                  }}
-                  defaultQuery={orderData.to_location.address}
-                  customInput={CustomInput}
+              <div className="form-group">
+                <label htmlFor="recipient.phones[0].number">
+                  * Номер телефона:
+                </label>
+                <Field
+                  type="text"
+                  id="recipient.phones[0].number"
+                  name="recipient.phones[0].number"
+                  className={`form-control ${
+                    errors.recipient?.phones?.[0] &&
+                    touched.recipient?.phones?.[0]?.number
+                      ? "error"
+                      : ""
+                  }`}
+                  placeholder={
+                    touched.recipient?.phones?.[0]?.number &&
+                    errors.recipient?.phones?.[0]
+                      ? "Номер телефона обязателен"
+                      : 'Например: "+7 (999) 999-99-99"'
+                  }
                 />
               </div>
-            </div>
 
-            <div className="form-group">
-              <label>Комментарий к заказу:</label>
-              <span className="comment">{orderData.comment}</span>
-            </div>
+              <div className="form-group">
+                <label htmlFor="to_location.address">* Адрес получателя:</label>
+                <div
+                  className={`form-control address ${
+                    errors.to_location?.address && touched.to_location?.address
+                      ? "error"
+                      : ""
+                  }`}
+                >
+                  <AddressSuggestions
+                    token={apiKey}
+                    onChange={(suggestion: any) => {
+                      setFieldValue("to_location.city", suggestion.data.city);
+                      setFieldValue(
+                        "to_location.postal_code",
+                        suggestion.data.postal_code
+                      );
+                      setFieldValue("to_location.address", suggestion.value);
+                    }}
+                    inputProps={{
+                      placeholder: "Введите адрес",
+                    }}
+                    defaultQuery={orderData.to_location.address}
+                    customInput={CustomInput}
+                  />
+                </div>
+              </div>
 
-            <div style={{ color: "red" }}>
-              <p style={{ marginBottom: 14 }}>* - обязательные поля</p>
-            </div>
+              <div className="form-group">
+                <label>Комментарий к заказу:</label>
+                <span className="comment">{orderData.comment}</span>
+              </div>
 
-            <button className="btn" type="submit">
-              Далее
-            </button>
-          </Form>
-        )}
-      </Formik>
+              <div style={{ color: "red" }}>
+                <p style={{ marginBottom: 14 }}>* - обязательные поля</p>
+              </div>
+
+              <button className="btn" type="submit">
+                Далее
+              </button>
+            </Form>
+          )}
+        </Formik>
+      )}
     </div>
   );
 };
