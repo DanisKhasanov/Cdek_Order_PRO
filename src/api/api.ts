3ca62407ba77230bf4f7240ada6c0eaeffa93456
DanisKhasanov <danis.khasanov.95@gmail.com>
2024-11-07 15:45:28 +0300

    test api

--- src/api/api.ts
+++ src/api/api.ts
@@ -10,10 +10,10 @@ const api = axios.create({
   withCredentials: true,
 });
 
-// api.interceptors.request.use((config) => {
-//   config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
-//   return config;
-// });
+api.interceptors.request.use((config) => {
+  config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
+  return config;
+});
 
 api.interceptors.response.use(
   (config) => {
