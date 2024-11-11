a2c56009ccf430003e660d7f7533446f4d21b694
DanisKhasanov <danis.khasanov.95@gmail.com>
2024-11-11 10:29:55 +0300

    обновил конфиг vite

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
-import fs from 'fs';
-// https://vitejs.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  server: {
-    https: {
-      key: fs.readFileSync('./.cert/key.pem'),
-      cert: fs.readFileSync('./.cert/cert.pem'),
-    },
-  },
+
   resolve: {
     alias: {
-      '@': '/src',
+      "@": "/src",
     },
   },
-})
+});
