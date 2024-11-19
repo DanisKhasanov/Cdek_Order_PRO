dbed28d2c400d719534c4e3b84d061c87a060ffd
DanisKhasanov <danis.khasanov.95@gmail.com>
2024-11-19 16:13:16 +0300

    11

--- src/api/api.ts
+++ src/api/api.ts
@@ -236,7 +236,7 @@ export const RequestTemplateTariff = (orderData: any) => ({
 
 export const PostSettingAccount = async (payload: any) => {
   try {
-    const response = await api.get(`/api/moysklad/context/${payload.contextKey}`);
+    const response = await api.get(`/moysklad/context/${payload.contextKey}`);
     return response.data;
   } catch (error) {
     console.error("Ошибка при отправке данных на сервер:", error);
