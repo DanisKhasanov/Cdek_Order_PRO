17026948cd1fd49d50797a313258cf22302595bf
Danis Khasanov <danis.khasanov.95@gmail.com>
2025-01-10 16:03:11 +0300

    Добавление поля postalCode в объект fromLocation для передачи данных о местоположении

--- src/api/requestTemplate/RequestTemplateTariff.ts
+++ src/api/requestTemplate/RequestTemplateTariff.ts
@@ -5,6 +5,7 @@ const { orderType } = JSON.parse(
 export const RequestTemplateTariff = (orderData: any) => ({
   fromLocation: {
     code: orderData.fromLocation?.code,
+    postalCode: orderData.fromLocation?.postalCode,
   },
   type: orderType,
   toLocation: {
